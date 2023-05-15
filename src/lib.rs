bcc4a84cb39567505cda51ac3b411c753637e28d
Neil Danson <neil.danson@gmail.com>
2023-05-15 15:47:45 +0100

    Big one. Database. Migrations. Code structure cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,7 @@
-use std::net::TcpListener;
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
-use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-pub async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
 
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
 
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
