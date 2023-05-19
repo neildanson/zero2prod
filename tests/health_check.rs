5050dda26300481bb7dc02d0d6a8953020bd3d01
Neil Danson <neil.danson@gmail.com>
2023-05-19 11:57:15 +0100

    Incude tracing in tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,14 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::DatabaseSettings;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let subscriber = get_subscriber("test".into(), "debug".into());
+    init_subscriber(subscriber);
+});
 
 #[tokio::test]
 async fn health_check_works() {
@@ -82,6 +89,8 @@ pub struct TestApp {
 
 // Launch our application in the background ~somehow~
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
